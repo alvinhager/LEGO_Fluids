a9b2a1395e2401493f586c0a596e481294d4292e
Alvin H <alvinhager@hotmail.com>
2021-05-13 22:19:44 +0200

    Added simple water particle struct class

---
+++ WaterParticle.h
@@ -0,0 +1,16 @@
+#ifndef Water_Particle_AH
+#define Water_Particle_AH
+
+#include "VectorMath.h"
+
+struct WaterParticle
+{
+    VectorMath::vec3 velocity;
+    VectorMath::vec3 position;
+
+    WaterParticle() {}
+    WaterParticle(VectorMath::vec3 pos) : position(pos) {}
+    WaterParticle(VectorMath::vec3 pos, VectorMath::vec3 vel) : position(pos), velocity(vel) {}
+};
+
+#endif
